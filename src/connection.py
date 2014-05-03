e41164176fd25848761a9c269b4d519ebc7425ce
ckoryom <ckoryom@gmail.com>
2014-05-03 20:07:13 +0200

    Closes #3 and Closes #1

--- src/connection.py
+++ src/connection.py
@@ -2,6 +2,7 @@
 
 import urllib2
 import json
+from libxml2 import isDigit
 
 class Connection:
 	apiUrl = "https://api.github.com"
@@ -13,12 +14,23 @@ class Connection:
 	gitHubAccount = ""
 	gitHubRepository = ""
 	response = ""
-	def constructUrl(self, type):
+	issueState = "closed"
+	totalPages = 1
+	currentPage = 1
+	type = ""
+	
+	def constructUrl(self, type, data):
 		url = ""
 		url += self.apiUrl + "/repos/"
 		url += self.gitHubAccount + "/" + self.gitHubRepository + "/"
-		url += type + "?page=" + str(self.page) + "&per_page=" + str(self.perPage)
-		url += "&auth_token=" + self.token
+		if type == "issues":
+			url += type + "?state=" + self.issueState
+		elif type == "events":
+			url += "issues/" + data + "/" + type
+		elif type == "commits":
+			url += type + "/" + data
+		url += "?page=" + str(self.page) + "&per_page=" + str(self.perPage)
+		url += "&access_token=" + self.token
 		self.url = url
 
 	def getHeaderValue(self, headers, key):
@@ -34,16 +46,31 @@ class Connection:
 					link = link.replace("<","")
 					link = link.replace(">","")
 					link = link.replace(" ","")
+					values = str(item[2]).split(";")
+					linkLastPage = str(values[0]).replace("'","")
+					linkLastPage = linkLastPage.replace("<","")
+					linkLastPage = linkLastPage.replace(">","")
+					linkLastPage = linkLastPage.replace(" ","")
+					lastPage = linkLastPage.find("&page=")
+					if lastPage >= 0:
+						totalPages = ""
+						for i in range(lastPage, len(linkLastPage)):
+							if linkLastPage[i].isdigit():
+								totalPages += linkLastPage[i]
+						self.totalPages = int(totalPages)
 					return link
+				
 
-	def requestType(self, type, next):
+	def requestType(self, type, data, next):
 		if next == False:
-			self.constructUrl(type)
+			self.constructUrl(type, data)
 		else:
 			self.url = self.nextUrl
+			self.currentPage += 1
 		self.response = urllib2.urlopen(self.url)
 		headers = self.response.info().items()
 		self.nextUrl = self.getHeaderValue(headers,"link")
+		self.type = type
 
 	def getResponseJson(self):
 		return json.load(self.response)
--- src/mining.py
+++ src/mining.py
@@ -9,7 +9,12 @@ class Mining:
 	gitHubRepository = ""
 	token = ""
 	dataTree = ""
-
+	
+	def getProgressPercentage(self, currentCount, connection):
+		totalItems = connection.perPage * connection.totalPages
+		percent = (currentCount*100)/totalItems
+		print(connection.type + ": " + str(currentCount) + " of " + str(totalItems) +  " - "  + str(percent) + "% of " + connection.type)
+	
 	def startMiningProcedure(self):
 		self.gitHubAccount = raw_input("Enter GitHub account:")
 		self.gitHubRepository = raw_input("Enter GitHub repository:")
@@ -20,14 +25,52 @@ class Mining:
 		self.dataTree.defineTree()
 		self.dataTree.saveTree("/xml/" + self.gitHubRepository +".xml")
 
+	def mineCommits(self, sha, issueElement):
+		commitsConnection = Connection()
+		commitsConnection.gitHubAccount = self.gitHubAccount
+		commitsConnection.gitHubRepository = self.gitHubRepository
+		commitsConnection.token = self.token
+		commitsConnection.requestType("commits", sha, False)
+		commit = commitsConnection.getResponseJson()
+		commitElement = self.dataTree.addSubElement(issueElement, "commit")
+		commitElement.set("sha", sha)
+		try:
+			commitElement.set("message", str(commit["commit"]["message"]))
+			pass
+		except Exception, e:
+			print "Could not get Message from sha# " + str(sha)
+		for file in commit["files"]:
+			fileElement = self.dataTree.addSubElement(commitElement, "file")
+			fileElement.set("filename", str(file["filename"]))
+		self.dataTree.defineTree()
+		self.dataTree.saveTree("/xml/" + self.gitHubRepository +".xml")
+			
+			
+	def mineEvents(self, issue, issueElement):
+		eventsConnection = Connection()
+		eventsConnection.gitHubAccount = self.gitHubAccount
+		eventsConnection.gitHubRepository = self.gitHubRepository
+		eventsConnection.token = self.token
+		eventsConnection.requestType("events", str(issue["number"]), False)
+		events = eventsConnection.getResponseJson()
+		count = 0
+		for event in events:
+			self.getProgressPercentage(count, eventsConnection)
+			if str(str(event["commit_id"])) != "None":
+				self.mineCommits(str(event["commit_id"]), issueElement)
+			count += 1
+				
+	
 	def mineIssues(self):
 		issuesConnection = Connection()
 		issuesConnection.gitHubAccount = self.gitHubAccount
 		issuesConnection.gitHubRepository = self.gitHubRepository
 		issuesConnection.token = self.token
-		issuesConnection.requestType("issues",False)
+		issuesConnection.requestType("issues", "", False)
 		issues = issuesConnection.getResponseJson()
+		count = 0
 		for issue in issues:
+			self.getProgressPercentage(count, issuesConnection)
 			issueElement = self.dataTree.addSubElement(self.dataTree.root, "issue")
 			issueElement.set("id", str(issue["number"]))
 			issueElement.set("state", str(issue["state"]))
@@ -35,4 +78,11 @@ class Mining:
 				issueElement.set("title", str(issue["title"]))
 				pass
 			except Exception, e:
-				print "Could not get Titlte from Issue# " + str(issue["number"])
+				print "Could not get Title from Issue# " + str(issue["number"])
+			self.mineEvents(issue, issueElement)
+			count += 1
+	
+	
+		
+		
+				
